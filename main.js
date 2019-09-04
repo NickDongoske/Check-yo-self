4a78ef7461f5fe9138990d271b582fd7e097bc87
Dustin Mikusko <dustinjm713@yahoo.com>
2019-09-04 11:59:46 -0600

    Add ToDoList Card

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
+// var ToDoList = require('./todo-list');
+// var Task = requires('task');
+
 var taskItemInput = document.getElementById('left-side-task-input');
+var leftSideTitleInput = document.getElementById('left-side-title-input').value;
 var plusButton = document.getElementById('plus-button');
 var taskList = document.querySelector('.task-list');
+var makeTaskList = document.querySelector('#make-task-list');
+var currentId = 0;
+var taskArray = document.querySelectorAll('.task-item');
+var rightSideCards = document.querySelector('.right-side-cards');
+
 
 plusButton.addEventListener('click', addTaskHandler);
 
+makeTaskList.addEventListener('click', makeToDoList);
+
+
+function makeToDoList() {
+  var toDoList = new ToDoList(currentId, leftSideTitleInput, taskArray);
+  rightSideCards.insertAdjacentHTML('afterbegin', '<section class="to-do-card"><div class="task-title"><h3>Task Title</h3><hr class="task-card-line"/></div><div class"task-checklist"><ul class="checklist"><li><input class="to-do-checkbox" type="checkbox" name="Task Completed" value="">Hello</li></ul><hr class="task-card-line"/></div><div class="task-btns"><div class="btn-group"><img class="urgency" src="images/urgent.svg" alt="Lightning Bolt To Show Urgency"><figcaption>Urgent</figcaption></div><div class="btn-group"><img class="delete-card"src="images/delete.svg" alt="Delete Button"><figcaption>Delete</figcaption></div></div></section>');
+  currentId++;
+}
 
 
 function addTaskHandler() {
@@ -28,7 +45,7 @@ function removeError(input) {
 }
 
 function createTaskItem() {
-  taskList.insertAdjacentHTML('beforeend', '<li><button type="button" name="delete-button" class="delete-button"><img class="delete-button" src="images/delete.svg" alt="Delete Button for Task Item"></button><span class="new-task-item">Added</span></li>');
+  taskList.insertAdjacentHTML('beforeend', '<li class="task-item"><button type="button" name="delete-button" class="delete-button"><img class="delete-button" src="images/delete.svg" alt="Delete Button for Task Item"></button><span class="new-task-item">Added</span></li>');
   document.querySelector('.new-task-item').innerText = `${taskItemInput.value}`;
   taskItemInput.value = '';
   document.querySelector('.new-task-item').classList.remove('new-task-item');
---
+++ todo-list.js
@@ -0,0 +1,15 @@
+class ToDoList {
+  constructor (id, title, taskArray) {
+    this.id = id;
+    this.title = title;
+    this.urgent = false;
+    this.taskArray = taskArray;
+  }
+
+  // updateToDo () {
+  //   update urgency to false;
+  //   update title
+  // }
+}
+
+// module.exports = ToDoList;
