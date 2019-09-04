3577be23cc58b9ba942d980b37449b7c4178dfdb
Dustin Mikusko <dustinjm713@yahoo.com>
2019-09-04 16:40:03 -0600

    Add clear button functionality and delete task functions

--- main.js
+++ main.js
@@ -2,24 +2,36 @@
 // var Task = requires('task');
 
 var taskItemInput = document.getElementById('left-side-task-input');
-var leftSideTitleInput = document.getElementById('left-side-title-input').value;
+var leftSideTitleInput = document.getElementById('left-side-title-input');
 var plusButton = document.getElementById('plus-button');
 var taskList = document.querySelector('.task-list');
 var makeTaskList = document.querySelector('#make-task-list');
 var currentId = 0;
-var taskArray = document.querySelectorAll('.task-item');
+var taskArray = [];
 var rightSideCards = document.querySelector('.right-side-cards');
+var leftSideForm = document.getElementById('left-side-form');
 
 
 plusButton.addEventListener('click', addTaskHandler);
 
 makeTaskList.addEventListener('click', makeToDoList);
 
+taskList.addEventListener('click', deleteTaskItem);
+
+leftSideForm.addEventListener('click', clearAll);
 
 function makeToDoList() {
-  var toDoList = new ToDoList(currentId, leftSideTitleInput, taskArray);
-  rightSideCards.insertAdjacentHTML('afterbegin', '<section class="to-do-card"><div class="task-title"><h3>Task Title</h3><hr class="task-card-line"/></div><div class"task-checklist"><ul class="checklist"><li><input class="to-do-checkbox" type="checkbox" name="Task Completed" value="">Hello</li></ul><hr class="task-card-line"/></div><div class="task-btns"><div class="btn-group"><img class="urgency" src="images/urgent.svg" alt="Lightning Bolt To Show Urgency"><figcaption>Urgent</figcaption></div><div class="btn-group"><img class="delete-card"src="images/delete.svg" alt="Delete Button"><figcaption>Delete</figcaption></div></div></section>');
+  var toDoList = new ToDoList(currentId, `${leftSideTitleInput.value}`, taskArray);
+  rightSideCards.insertAdjacentHTML('afterbegin', `
+    <section class="to-do-card">
+      <div class="task-title"><h3 class="card-title">Task Title</h3><hr class="task-card-line"/></div><div class"task-checklist"><ul class="checklist">${makeList(taskArray)}<li><input class="to-do-checkbox" type="checkbox" name="Task Completed" value="">Hello</li></ul><hr class="task-card-line"/></div><div class="task-btns"><div class="btn-group"><img class="urgency" src="images/urgent.svg" alt="Lightning Bolt To Show Urgency"><figcaption>Urgent</figcaption></div><div class="btn-group"><img class="delete-card"src="images/delete.svg" alt="Delete Button"><figcaption>Delete</figcaption></div></div></section>`);
+  document.querySelector('.card-title').innerText = `${leftSideTitleInput.value}`;
   currentId++;
+}
+
+function makeList(array) {
+  console.log(`oh hey backticks`);
+  return 'not hello';
 }
 
 
@@ -45,8 +57,23 @@ function removeError(input) {
 }
 
 function createTaskItem() {
-  taskList.insertAdjacentHTML('beforeend', '<li class="task-item"><button type="button" name="delete-button" class="delete-button"><img class="delete-button" src="images/delete.svg" alt="Delete Button for Task Item"></button><span class="new-task-item">Added</span></li>');
+  taskList.insertAdjacentHTML('beforeend', '<li class="list-item"><button type="button" name="delete-button" class="delete-button"><img class="delete-button" src="images/delete.svg" alt="Delete Button for Task Item"></button><span class="new-task-item task-item">Added</span></li>');
   document.querySelector('.new-task-item').innerText = `${taskItemInput.value}`;
+  taskArray.push(`${taskItemInput.value}`);
   taskItemInput.value = '';
   document.querySelector('.new-task-item').classList.remove('new-task-item');
+}
+
+function deleteTaskItem(event) {
+  if (event.target.classList.contains('delete-button')) {
+    event.target.closest('li').remove();
+  }
+}
+
+function clearAll(event) {
+  if(event.target.classList.contains('clear-all')) {
+    taskItemInput.value = '';
+    leftSideTitleInput.value = '';
+    taskList.innerHTML = '';
+  }
 }
